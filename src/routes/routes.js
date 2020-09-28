3a2f04cee39d812eefcc8794637805c7a445ea92
Christiano Bezerra Peres <christianob@toledobrasil.com.br>
2020-09-28 16:31:29 -0300

    Backend Insert

---
+++ src/routes/routes.js
@@ -0,0 +1,5 @@
+const express = require(' express ');
+const UsuarioController = require(' ../controllers/usuarioController.js ');
+const router = express.Router();
+router.post(' /usuarios ', UsuarioController.Insert);
+module.exports = router;
